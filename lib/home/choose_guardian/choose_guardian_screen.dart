876099e8191433f8076d022b4dbe475f7b6f83d9
Altureny <altureny17@gmail.com>
2025-12-28 23:10:31 +0300

    Update choose_guardian_screen.dart

--- lib/home/choose_guardian/choose_guardian_screen.dart
+++ lib/home/choose_guardian/choose_guardian_screen.dart
@@ -3,7 +3,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
-import 'package:trace/ui/text_with_tap.dart';
+import 'package:juodylive/ui/text_with_tap.dart';
 
 import '../../helpers/quick_actions.dart';
 import '../../helpers/quick_help.dart';
