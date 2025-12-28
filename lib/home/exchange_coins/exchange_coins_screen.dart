d29ee38707b1ffbc8be214730608e57580ac8cdd
Altureny <altureny17@gmail.com>
2025-12-29 00:04:20 +0300

    Update exchange_coins_screen.dart

--- lib/home/exchange_coins/exchange_coins_screen.dart
+++ lib/home/exchange_coins/exchange_coins_screen.dart
@@ -3,7 +3,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
-import 'package:trace/ui/container_with_corner.dart';
+import 'package:juodylive/ui/container_with_corner.dart';
 
 import '../../helpers/quick_help.dart';
 import '../../models/UserModel.dart';
