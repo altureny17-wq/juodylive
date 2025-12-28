226beb95b1958186d7558d932ae0ac6ca16ef74d
Altureny <altureny17@gmail.com>
2025-12-28 22:59:30 +0300

    Update agency_group_creation_screen.dart

--- lib/home/agency/agency_group_creation_screen.dart
+++ lib/home/agency/agency_group_creation_screen.dart
@@ -5,15 +5,15 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
-import 'package:trace/helpers/quick_actions.dart';
-import 'package:trace/helpers/quick_help.dart';
-import 'package:trace/models/GroupMessageModel.dart';
-import 'package:trace/models/MessageListModel.dart';
-import 'package:trace/models/MessageModel.dart';
-import 'package:trace/models/UserModel.dart';
-import 'package:trace/ui/container_with_corner.dart';
-import 'package:trace/ui/text_with_tap.dart';
-import 'package:trace/utils/colors.dart';
+import 'package:juodylive/helpers/quick_actions.dart';
+import 'package:juodylive/helpers/quick_help.dart';
+import 'package:juodylive/models/GroupMessageModel.dart';
+import 'package:juodylive/models/MessageListModel.dart';
+import 'package:juodylive/models/MessageModel.dart';
+import 'package:juodylive/models/UserModel.dart';
+import 'package:juodylive/ui/container_with_corner.dart';
+import 'package:juodylive/ui/text_with_tap.dart';
+import 'package:juodylive/utils/colors.dart';
 
 import '../../models/AgencyMembersModel.dart';
 
