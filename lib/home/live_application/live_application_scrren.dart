56647c0c049517d48e639aab70e6dbc68bdb1874
Altureny <altureny17@gmail.com>
2025-12-29 16:45:14 +0300

    Update live_application_scrren.dart

--- lib/home/live_application/live_application_scrren.dart
+++ lib/home/live_application/live_application_scrren.dart
@@ -2,8 +2,8 @@
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:trace/models/UserModel.dart';
-import 'package:trace/ui/container_with_corner.dart';
+import 'package:juodylive/models/UserModel.dart';
+import 'package:juodylive/ui/container_with_corner.dart';
 
 import '../../app/setup.dart';
 import '../../helpers/quick_help.dart';
