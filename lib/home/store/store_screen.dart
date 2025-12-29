503ba994a5734f41d6f455a3e91a755ef8a912bf
Altureny <altureny17@gmail.com>
2025-12-29 23:09:16 +0300

    Update store_screen.dart

--- lib/home/store/store_screen.dart
+++ lib/home/store/store_screen.dart
@@ -3,8 +3,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
-import 'package:trace/helpers/quick_actions.dart';
-import 'package:trace/home/store/wave.dart';
+import 'package:juodylive/helpers/quick_actions.dart';
+import 'package:juodylive/home/store/wave.dart';
 
 import '../../app/setup.dart';
 import '../../helpers/quick_help.dart';
