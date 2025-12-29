d39e3614fe67a7711ab652280f8466241e593252
Altureny <altureny17@gmail.com>
2025-12-29 17:44:39 +0300

    Update privilege_info_screen.dart

--- lib/home/mvp/privilege_info_screen.dart
+++ lib/home/mvp/privilege_info_screen.dart
@@ -2,9 +2,9 @@
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:trace/ui/container_with_corner.dart';
-import 'package:trace/ui/text_with_tap.dart';
-import 'package:trace/utils/colors.dart';
+import 'package:juodylive/ui/container_with_corner.dart';
+import 'package:juodylive/ui/text_with_tap.dart';
+import 'package:juodylive/utils/colors.dart';
 
 import '../../app/config.dart';
 import '../../helpers/quick_help.dart';
