a41d50c408654be45f8d25bc6646665f95010801
Altureny <altureny17@gmail.com>
2025-12-30 14:25:23 +0300

    Update GiftsSentModel.dart

--- lib/models/GiftsSentModel.dart
+++ lib/models/GiftsSentModel.dart
@@ -1,5 +1,5 @@
-import 'package:trace/models/GiftsModel.dart';
-import 'package:trace/models/UserModel.dart';
+import 'package:juodylive/models/GiftsModel.dart';
+import 'package:juodylive/models/UserModel.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 class GiftsSentModel extends ParseObject implements ParseCloneable {
