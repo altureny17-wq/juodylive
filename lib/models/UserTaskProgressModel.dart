718d7bc94b8d212bfc36b8673a611b4153416c0f
Altureny <altureny17@gmail.com>
2026-02-19 00:40:02 +0300

    Refactor UserTaskProgressModel getters and setters

--- lib/models/UserTaskProgressModel.dart
+++ lib/models/UserTaskProgressModel.dart
@@ -22,52 +22,42 @@ class UserTaskProgressModel extends ParseObject implements ParseCloneable {
   static String keyTask = "task";
   static String keyTaskId = "taskId";
   
-  static String keyProgress = "progress"; // الدقائق المنجزة
-  static String keyTarget = "target"; // الهدف بالدقائق
+  static String keyProgress = "progress";
+  static String keyTarget = "target";
   static String keyIsCompleted = "isCompleted";
   static String keyCompletedAt = "completedAt";
-  static String keyCompletionDate = "completionDate"; // تاريخ الإكمال
+  static String keyCompletionDate = "completionDate";
   
   static String keyRewardClaimed = "rewardClaimed";
   static String keyClaimedAt = "claimedAt";
-  
   static String keyLastUpdated = "lastUpdated";
 
-  // Getters & Setters
+  // Getters
   UserModel? get getUser => get<UserModel>(keyUser);
-  set setUser(UserModel user) => set<UserModel>(keyUser, user);
-
   String? get getUserId => get<String>(keyUserId);
-  set setUserId(String id) => set<String>(keyUserId, id);
-
   DailyTaskModel? get getTask => get<DailyTaskModel>(keyTask);
-  set setTask(DailyTaskModel task) => set<DailyTaskModel>(keyTask, task);
-
   String? get getTaskId => get<String>(keyTaskId);
-  set setTaskId(String id) => set<String>(keyTaskId, id);
-
   int? get getProgress => get<int>(keyProgress);
+  int? get getTarget => get<int>(keyTarget);
+  bool? get getIsCompleted => get<bool>(keyIsCompleted);
+  DateTime? get getCompletedAt => get<DateTime>(keyCompletedAt);
+  DateTime? get getCompletionDate => get<DateTime>(keyCompletionDate);
+  bool? get getRewardClaimed => get<bool>(keyRewardClaimed);
+  DateTime? get getClaimedAt => get<DateTime>(keyClaimedAt);
+  DateTime? get getLastUpdated => get<DateTime>(keyLastUpdated);
+
+  // Setters
+  set setUser(UserModel user) => set<UserModel>(keyUser, user);
+  set setUserId(String id) => set<String>(keyUserId, id);
+  set setTask(DailyTaskModel task) => set<DailyTaskModel>(keyTask, task);
+  set setTaskId(String id) => set<String>(keyTaskId, id);
   set setProgress(int minutes) => set<int>(keyProgress, minutes);
   set incrementProgress(int minutes) => setIncrement(keyProgress, minutes);
-
-  int? get getTarget => get<int>(keyTarget);
   set setTarget(int minutes) => set<int>(keyTarget, minutes);
-
-  bool? get getIsCompleted => get<bool>(keyIsCompleted);
   set setIsCompleted(bool completed) => set<bool>(keyIsCompleted, completed);
-
-  DateTime? get getCompletedAt => get<DateTime>(keyCompletedAt);
   set setCompletedAt(DateTime date) => set<DateTime>(keyCompletedAt, date);
-
-  DateTime? get getCompletionDate => get<DateTime>(keyCompletionDate);
   set setCompletionDate(DateTime date) => set<DateTime>(keyCompletionDate, date);
-
-  bool? get getRewardClaimed => get<bool>(keyRewardClaimed);
   set setRewardClaimed(bool claimed) => set<bool>(keyRewardClaimed, claimed);
-
-  DateTime? get getClaimedAt => get<DateTime>(keyClaimedAt);
   set setClaimedAt(DateTime date) => set<DateTime>(keyClaimedAt, date);
-
-  DateTime? get getLastUpdated => get<DateTime>(keyLastUpdated);
   set setLastUpdated(DateTime date) => set<DateTime>(keyLastUpdated, date);
 }
