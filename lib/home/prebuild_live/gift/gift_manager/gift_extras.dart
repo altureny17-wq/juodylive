7cb756cbae23da51cbf43c856f0999d018dd703c
Altureny <altureny17@gmail.com>
2026-03-21 12:18:42 +0300

    Add files via upload

---
+++ lib/home/prebuild_live/gift/gift_manager/gift_extras.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+/// بيانات تأثير الدخول المستقبَل
+class ZegoEntranceEffectItem {
+  final String fileUrl;
+  final String senderUserID;
+  final String senderUserName;
+
+  ZegoEntranceEffectItem({
+    required this.fileUrl,
+    required this.senderUserID,
+    required this.senderUserName,
+  });
+}
+
+/// بيانات الرسالة العائمة المستقبَلة
+class ZegoFloatMessageItem {
+  final String text;
+  final String senderUserID;
+  final String senderUserName;
+  final String avatarUrl;
+
+  ZegoFloatMessageItem({
+    required this.text,
+    required this.senderUserID,
+    required this.senderUserName,
+    required this.avatarUrl,
+  });
+}
